70f6639138f08a5b1e2591036c6bb82979be5372
segfault16 <sebastian.halder@gmx.de>
2019-01-25 08:16:49 +0100

    Move VisGraph to components

---
+++ server/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import NavBar from './components/NavBar'
+import VisGraph from './components/VisGraph'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div id="content">
+        <NavBar />
+        <VisGraph />
+        <div></div>
+      </div>
+    );
+  }
+}
+
+export default App;
