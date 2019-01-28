1edc819a24dba0b21d1a73f5324ac6bacc692567
segfault16 <sebastian.halder@gmx.de>
2019-01-28 06:13:32 +0100

    First step towards own configurator

--- server/src/components/NodePopup.js
+++ server/src/components/NodePopup.js
@@ -1,4 +1,11 @@
 import React from "react";
+import Button from '@material-ui/core/Button';
+import Slider from '@material-ui/lab/Slider';
+import FormControl from '@material-ui/core/FormControl'
+import FormGroup from '@material-ui/core/FormGroup'
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import FormControlLabel from '@material-ui/core/FormControlLabel'
 import FilterGraphService from "../services/FilterGraphService";
 import './NodePopup.css'
 var Configurator = require("vis/lib/shared/Configurator").default;
@@ -56,7 +63,11 @@ class NodePopup extends React.Component {
             mode: props.mode,
             nodeUid: props.nodeUid,
             onSave: props.onSave,
-            onCancel: props.onCancel
+            onCancel: props.onCancel,
+            config: {
+                parameters: [],
+                values: []
+            }
         }
     }
 
@@ -94,6 +105,16 @@ class NodePopup extends React.Component {
             Promise.all([stateJson, json]).then(result => {
                 var effect = result[0]["py/state"]["effect"]["py/state"];
                 var values = result[1];
+                console.log(effect)
+                console.log(values)
+                this.setState(state => {
+                    return {
+                        config: {
+                            parameters: values.parameters,
+                            values: effect
+                        }
+                    }
+                })
                 configurator = new ConfigurationWrapper(uid, document.getElementById('node-configuration'), values, effect, async (nodeUid, data) => {
                     console.log("emitting", data['parameters']);
                     await FilterGraphService.updateNode(nodeUid, data['parameters'])
@@ -198,7 +219,32 @@ class NodePopup extends React.Component {
         return;
     }
 
+    handleChange = (value, parameter) => {
+      };
+
     render() {
+        console.log(Object.keys(this.state.config.parameters))
+        let parameters = this.state.config.parameters;
+        let values = this.state.config.values;
+        let configList = Object.keys(parameters).map( (data, index) => {
+            return (
+                <Grid container spacing={24}>
+                
+                {/* <div className="vis-configuration vis-config-item vis-config-s2"> */}
+                <Grid item xs={3}>
+                {data}:
+                </Grid>
+                <Grid item xs={7}>
+                    <Slider id={data} value={values[data]} min={parameters[data][1]} max={parameters[data][2]} step={parameters[data][3]} onChange={(e, val)=>this.handleChange(val, data)}/> 
+                </Grid>
+                <Grid item xs={2}>
+                {values[data]}
+                </Grid>
+                {/* </div> */}
+                
+                </Grid>
+            )
+        });
         return (
             <div id="node-popUp">
                 <h2 id="node-operation">{this.state.mode}</h2>
@@ -207,11 +253,14 @@ class NodePopup extends React.Component {
                     <div className="vis-configuration vis-config-item vis-config-s2"><select className="form-control" id='node-effectDropdown' name='node-effectDropdown'></select></div>
                 </div>
                 <div id="node-configuration"></div>
+                <div id="node-grid">
+                {configList}
+                </div>
                 <table style={{ margin: "auto" }}>
                     <tbody>
                         <tr>
-                            <td><input type="button" value="save" id="node-saveButton" onClick={this.handleNodeEditSave} /></td>
-                            <td><input type="button" value="cancel" id="node-cancelButton" onClick={this.handleNodeEditCancel} /></td>
+                            <td><Button variant="contained" id="node-saveButton" onClick={this.handleNodeEditSave}>save</Button></td>
+                            <td><Button variant="contained" id="node-cancelButton" onClick={this.handleNodeEditCancel}>cancel</Button></td>
                         </tr>
                     </tbody>
                 </table>
