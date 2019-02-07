a1570a4c3827849bd2821b8e2b7c2c414798854f
segfault16 <sebastian.halder@gmx.de>
2019-02-07 05:21:15 +0100

    Add Configurator to ConfigurationPage

--- server/src/pages/ConfigurationPage.js
+++ server/src/pages/ConfigurationPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 
+import Configurator from '../components/Configurator'
 import ProjectService from '../services/ProjectService'
 
 const styles = theme => ({
@@ -9,9 +10,26 @@ const styles = theme => ({
 });
 
 class ConfigurationPage extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            parameters: {},
+            values: {}
+        }
+    }
+
+    handleParameterChange = (parameter, value) => {
+
+    }
+
     render() {
         return (
             <React.Fragment>
+                <Configurator
+                    parameters={this.state.parameters}
+                    values={this.state.values}
+                    onChange={(parameter, value) => this.handleParameterChange(parameter, value)}
+                />
             </React.Fragment>
         )
     }
