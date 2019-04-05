b0918d475bfadd6b9615a9d2018df6efcb5c9441
segfault16 <sebastian.halder@gmx.de>
2019-04-05 05:46:12 +0200

    Remove destructive error handling

--- audioled/filtergraph.py
+++ audioled/filtergraph.py
@@ -27,12 +27,9 @@ class Node(object):
         self.numOutputChannels = self.effect.numOutputChannels()
 
     def __initstate__(self):
-        try:
-            self.effect.numOutputChannels()
-        except Exception:
-            print("Node effect not present. Replacing with Defense Mode effect.")
-            self.effect = generative.DefenceMode()
-
+        
+        self.effect.numOutputChannels()
+        
         self._outputBuffer = [None for i in range(0, self.effect.numOutputChannels())]
         self._inputBuffer = [None for i in range(0, self.effect.numInputChannels())]
         self._incomingConnections = []
