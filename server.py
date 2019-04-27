9714ad0f3ecdc2dc430b4e649965e28f9b95568b
fruechtegott <schoch.christoph@googlemail.com>
2019-04-27 14:40:09 +0200

    Added -DCS command for --device_candy_server
    For faster proxy to proxy to proxy

--- server.py
+++ server.py
@@ -573,7 +573,7 @@ if __name__ == '__main__':
         choices=[deviceRasp, deviceCandy],
         help='device to send RGB to (default: FadeCandy)')
     parser.add_argument(
-        '--device_candy_server', dest='device_candy_server', default=None, help='Server for device FadeCandy')
+        '--device_candy_server', '-DCS', dest='device_candy_server', default=None, help='Server for device FadeCandy')
     parser.add_argument(
         '--device_panel_mapping', dest='device_panel_mapping', default=None, help='Mapping file for panels')
     parser.add_argument(
