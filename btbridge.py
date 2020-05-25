b87263bfda1577f8748f3639aa8b9bdf18ec5ec0
segfault16 <sebastian.halder@gmx.de>
2020-05-25 10:18:47 +0100

    Add separate script for running a bluetooth bridge

---
+++ btbridge.py
@@ -0,0 +1,53 @@
+
+import logging
+import mido
+import sys
+import mido.backends.rtmidi  # Pyupdate required
+from audioled_controller import bluetooth
+
+logger = logging.getLogger(__name__)
+orig_factory = logging.getLogRecordFactory()
+
+
+def record_factory(*args, **kwargs):
+    record = orig_factory(*args, **kwargs)
+    record.sname = record.name[-10:] if len(record.name) > 10 else record.name
+    if record.threadName and len(record.threadName) > 10:
+        record.sthreadName = record.threadName[:10]
+    elif not record.threadName:
+        record.sthreadName = ""
+    else:
+        record.sthreadName = record.threadName
+    return record
+
+
+logging.setLogRecordFactory(record_factory)
+logging.basicConfig(stream=sys.stdout,
+                    level=logging.INFO,
+                    format='[%(relativeCreated)6d %(sthreadName)10s  ] %(sname)10s:%(levelname)s %(message)s')
+
+midi_out = None
+
+def callback(msg: mido.Message):
+    global midi_out
+    logger.info("Bluetooth received {}".format(msg))
+    if midi_out is None:
+        try:
+            logger.info("Connecting to existing port")
+            midi_out = mido.open_output('MOLECOLE Control In')
+        except Exception as e:
+            logger.error(e)
+    if midi_out is not None:
+        logger.info("Relay {}".format(msg))
+        midi_out.send(msg)
+
+
+if __name__ == '__main__':
+    logger.info("Advertising bluetooth")
+    bt = bluetooth.MidiBluetoothService(callback=callback, advertiseName='MOLECOLE Control')
+    logger.info("Creating virtual MIDI port")
+    midi_in = mido.open_input('MOLECOLE Control Out', virtual=True)
+    for msg in midi_in:
+        logger.info("Received {}".format(msg))
+        if bt is not None:
+            bt.send(msg)
